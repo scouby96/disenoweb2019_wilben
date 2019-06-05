fb5e8aaf798cf449511052a2601f04d8988082a6
scouby96 <wilben.alcema@gmail.com>
2019-06-05 12:32:18 -0400

    dia de javascript

---
+++ javascript_101/0_script.js
@@ -0,0 +1,22 @@
+alert("hola a todos");
+//pedir al usario que ingrese su edad
+var nombre=prompt("digite su nombre");
+var edad=prompt("digite su edad");
+console.log(nombre);
+console.log(edad);
+
+console.log(document.documentURI);
+console.log(document.body);
+console.log(document.head);
+console.log(document.title);
+console.log(document.URL);
+console.log(document.height);
+//esta variable no esta definida
+var pais
+console.log(pais)
+//definir el variable
+var pais="Haiti";
+console.log(pais);
+//cambiar el titulo
+document.title="nuevo titulo";
+
---
+++ javascript_101/perfil0.2/mi perfil.js
@@ -0,0 +1,6 @@
+//pedir el usario su nombre e edad
+var nombre=prompt("digite su nombre");
+var edad=prompt("digite su edad");
+document.write(`<h1>${nombre}</h1>`);
+document.write(`<h1>${edad}</h1>`);
+
